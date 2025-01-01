c2e8ebb1ee5cb9fc92de6d2549d5ec161914a669
Ahmed S <awesomedev43@gmail.com>
2025-01-01 13:56:50 -0500

    Forgot to commit file

---
+++ src/Navbar.tsx
@@ -0,0 +1,40 @@
+import { Component, For } from "solid-js";
+import { IconProgressCheck, IconBriefcase } from '@tabler/icons-solidjs';
+
+type ButtonProps = {
+    name: string,
+    icon: any
+};
+
+const Navbar: Component<{}> = () => {
+
+    const navButtons: ButtonProps[] = [
+        {
+            name: "Task",
+            icon: IconBriefcase,
+        },
+        {
+            name: "Completed",
+            icon: IconProgressCheck
+        }
+    ];
+
+
+    return (
+        <nav>
+            <For each={navButtons}>
+                {(item, _) =>
+
+                    <div class="flex flex-col text-center border-solid border-b-[0.5px] border-gray-300 hover:bg-blue-600 pt-1 pb-1 group">
+                        <div class="place-self-center">
+                            <item.icon size={30} class="group-hover:stroke-white"/>
+                        </div>
+                        <div class="p-1 text-md group-hover:text-white">{item.name}</div>
+                    </div>
+                }
+            </For>
+        </nav>
+    );
+};
+
+export default Navbar;
