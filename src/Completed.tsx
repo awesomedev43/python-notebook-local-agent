c85fee26653e48250a6e6000c5e20bd578b36d77
Ahmed S <awesomedev43@gmail.com>
2025-01-07 22:40:50 -0500

    Add logic for rendering completed table

--- src/Completed.tsx
+++ src/Completed.tsx
@@ -8,25 +8,41 @@ type CompletedJobData = {
     completed: number,
 }
 
-
 const Completed: Component<{}> = () => {
 
     const [init] = createResource(async (): Promise<CompletedJobData[]> => {
         const result: CompletedJobData[] = await invoke('get_all_completed', {});
-        console.log(result);
-        return result;
+        return result.reverse();
     });
 
+    const convertToDateString = (timestamp: number): string =>{
+        let d = new Date(timestamp*1000);
+        return d.toLocaleString()
+    };
 
-    return (<>
-        <h1>Completed</h1>
-        <For each={init()}>
-            {(item: CompletedJobData, _) => (
-                <p>{item.id}</p>  
-                                    
-            )}
-        </For>
-    </>);
+    return (
+        <div class="flex flex-col mr-4">
+            <table class="table-auto w-full pl-4 mb-4">
+                <thead>
+                    <tr class="bg-gray-200 text-black text-xl">
+                        <th class="py-1">Notebook</th>
+                        <th class="py-1">Completed</th>
+                    </tr>
+                </thead>
+                <tbody> 
+                    <For each={init()}>
+                        {(item, _) => (
+                            <tr class="text-center border p-3 text-md">
+                                <td class="py-1">{item.output_path}</td>
+                                <td class="py-1">{convertToDateString(item.completed)}</td>
+                            </tr>
+                        )}
+                    </For>
+                </tbody>
+            </table>
+        </div>
+
+    );
 };
 
 export default Completed;
